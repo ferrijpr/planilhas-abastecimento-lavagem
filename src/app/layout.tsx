663bde35deaf7c66a5dbda9b5d4a2dd0af2e6669
ferrijpr <redpandagame@gmail.com>
2025-10-06 19:30:33 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Criado com a Lasy",
-  description: "Projeto criado com Lasy AI",
+  title: "Ferri Milenium Internet",
+  description: "Projeto criado por Ferri",
 };
 
 export default function RootLayout({
